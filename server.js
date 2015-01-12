9d4a566bc94a8694f24ed27a1ad794463c265e8f
Shannon Byrne <s.byrne2254@gmail.com>
2015-01-11 19:40:26 -0600

    Simple chat functionality

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 var express = require('express');
-var app = express();
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-
-app.get('/', function(request, response) {
-    response.send('Hello world')
-});
+var app = express();
 var _ = require('underscore');
 
-var server = require('http').createServer(app);
-var sockets = require('socket.io');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var nodes = {};
 var usernames = {};
 
-server.listen(process.env.PORT || 3000);
+
 
 
 app.set('view engine', 'ejs');
+app.set('title', 'History Chat')
 app.set('view options', { layout: false });
-app.use(express.methodOverride());
-app.use(express.bodyParser());
-app.use(app.router);
+//app.use(express.methodOverride());
+//app.use(express.bodyParser());
+//app.use(app.router);
 
-app.use('/public', express.static('public'));
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.render('index');
+    //res.render('index');
+});
+
+server.listen(1137, function(){
+    console.log('Server running on port 1137');
+});
+
+io.on('connection', function(socket){
+    console.log("A user connected.")
+    socket.on('disconnect', function(){
+        console.log('A user disconnected.')
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    })
 });
 
 // BASIC NODE HELLO WORLD
